f155fb5d293a856c69f0d5ec0a13bced0a87b291
johancampion <johancampion13@gmail.com>
2018-11-27 15:20:08 +0100

    Maj du 27/11/2018

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -1,7 +1,119 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
+import java.util.Objects;
+
 /**
  * Created by pjvilloud on 21/09/17.
  */
 public class Employe {
+
+    private String nom;
+
+    private String prenom;
+
+    private String matricule;
+
+    private LocalDate dateEmbauche;
+
+    private Double salaire;
+
+    public Employe(){
+
+    }
+
+    public Employe(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matricule = matricule;
+        this.dateEmbauche = dateEmbauche;
+        this.salaire = salaire;
+    }
+
+    public String getMatricule() {
+        return matricule;
+
+    }
+
+    public void augmenterSalaire(Double augmentation){
+
+
+
+    }
+
+    public final Integer getNombreAnneeAnciennete(){
+
+        return  2018 - this.dateEmbauche.getYear();
+    }
+
+    static public Integer getNbConges(){
+        return Entreprise.NB_CONGES_BASE;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", matricule='" + matricule + '\'' +
+                ", dateEmbauche=" + dateEmbauche +
+                ", salaire=" + salaire +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employe)) return false;
+        Employe employe = (Employe) o;
+        return hashCode() == o.hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, prenom, matricule, dateEmbauche, salaire);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public LocalDate getDateEmbauche() {
+        return dateEmbauche;
+    }
+
+    public void setDateEmbauche(LocalDate dateEmbauche) throws Exception  {
+
+        if(dateEmbauche == null || dateEmbauche.isAfter(LocalDate.now()) ){
+            throw new Exception("La date d'embauche ne peut être postérieure à la date courante");
+        }
+
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    public Double getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(Double salaire) {
+        this.salaire = salaire;
+    }
 }
---
+++ src/main/java/com/ipiecoles/java/java220/Main.java
@@ -0,0 +1,11 @@
+package com.ipiecoles.java.java220;
+
+public class Main {
+
+    public static void main(String[] ars){
+        Employe employe = new Employe();
+
+        System.out.println(employe.toString());
+
+    }
+}
